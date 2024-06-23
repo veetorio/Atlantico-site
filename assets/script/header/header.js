bf23df91235be2cb3d14a87dce82d98953ea9150
Veetorio <ettorevitorio98@gmail.com>
2024-06-23 11:47:25 -0300

    melhoria em rotas

--- assets/script/header/header.js
+++ assets/script/header/header.js
@@ -4,11 +4,12 @@ const header = collection.children[0].children
 
 for (const iterator of header) {
     iterator.addEventListener("click", (e) => {
+        console.log(location.pathname)
         const origin = location.origin
         if(e.target.textContent === "home"){
             location.href = "../../index.html"
         }else{
-            location.href = origin + e.target.textContent + ".html"
+            location.href = origin + "/Atlantico-site/" + e.target.textContent + ".html"
         }
     })
 }
