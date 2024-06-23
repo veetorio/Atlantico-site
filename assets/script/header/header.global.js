29252a4e92256d1ccdf636232659dec84b297146
Veetorio <ettorevitorio98@gmail.com>
2024-06-23 11:44:10 -0300

    adição de origem nas path

--- assets/script/header/header.global.js
+++ assets/script/header/header.global.js
@@ -3,11 +3,14 @@ const collection = document.getElementsByClassName("content-text")[0]
 const header = collection.children[0].children
 
 for (const iterator of header) {
+
+
     iterator.addEventListener("click", (e) => {
+        const origin = location.origin
         if(e.target.textContent === "home"){
-            location.href = "index.html"
+            location.href = origin +  "/index.html"
         }else{
-            location.href = "/assets/pages/" + e.target.textContent + ".html"
+            location.href = origin + "/assets/pages/" + e.target.textContent + ".html"
         }
     })
 }
--- assets/script/header/header.js
+++ assets/script/header/header.js
@@ -4,10 +4,11 @@ const header = collection.children[0].children
 
 for (const iterator of header) {
     iterator.addEventListener("click", (e) => {
+        const origin = location.origin
         if(e.target.textContent === "home"){
             location.href = "../../index.html"
         }else{
-            location.href =  e.target.textContent + ".html"
+            location.href = origin + e.target.textContent + ".html"
         }
     })
 }
