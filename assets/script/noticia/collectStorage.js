b7f3eecc4a0e5e5b8efbc41919c4c62d2d15d2c1
Veetorio <ettorevitorio98@gmail.com>
2024-06-24 19:07:18 -0300

    algumas alterações

--- assets/script/noticia/collectStorage.js
+++ assets/script/noticia/collectStorage.js
@@ -1,3 +1,4 @@
+const btnBack = document.getElementById("back")
 function renderNoticy() {
     const element = JSON.parse(localStorage.getItem("card"))
     const img = document.getElementById("figure")
@@ -21,11 +22,20 @@ function renderNoticy() {
     }
     h1.textContent = element.h1;
     p.textContent = element.p;
-    img.style.background = `url(${element.img})`
+    img.style.background = `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)),
+    url(${element.img})`
     img.style.backgroundSize = `cover`
+    img.style.backgroundPosition = `50% 50%`
 
     localStorage.clear()
 
+}
+
+btnBack.addEventListener("click",returnPage)
+function returnPage(){
+    const origin = location.origin 
+    const path = "/assets/pages/noticias.html"
+    location.href = origin + path
 }
 
 renderNoticy()
--- assets/script/noticia/noticias.js
+++ assets/script/noticia/noticias.js
@@ -1,34 +1,35 @@
 const display = document.getElementById('newDisplay')
 class Card {
-    path;
-    desc;
-    titulo;
-    link;
-    constructor(titulo, desc, path) {
-        this.titulo = titulo;
-        this.desc = desc;
-        this.path = path;
+  path;
+  desc;
+  titulo;
+  link;
+  constructor(titulo, desc, path) {
+    this.titulo = titulo;
+    this.desc = desc;
+    this.path = path;
 
-    }
+  }
 }
 
 const noticias = [
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
-    new Card("test", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat blandit volutpat. In fringilla dignissim est ac dictum. Pellentesque quis.`,"../../../public/img/secondary.jpg"),
+  new Card("M11 3 X 1 ATLÂNTICO", `Neste último sábado (08/06) a Sociedade Esportiva Atlântico, em partida valida pela 2ª rodada da III Copa Interbairro de Futebol de Base, foi até o Estádio Beira Rio/Ilhinha.
+    Onde enfrentou a equipe do M11 nas categorias Sub15 e Sub17 `, "../../../public/img/noticias/img.jpeg"),
+  new Card("Estreia da Sociedade Esportiva Atlântico", `Estreia da Sociedade Esportiva Atlântico, sub15 e sub17, na III Copa Interbairro de Futebol de Base.`, "../../../public/img/noticias/img2.jpeg"),
+  new Card("Ações do Projeto Rede de Prosperidade Familiar, em parceria com o CIEDS/VALE", `Reunião de apresentação da nossa assistente social, Amanda, e alinhamento com as ações do projeto REDE DE PROSPERIDADE FAMILIAR e o CIEDS.
+Muitas novidades virão logo, logo!`, "../../../public/img/noticias/img3.jpeg"),
+  new Card("Escola de Futebol S.E.Atlantico, franquia oficial Prudentópolis Futebol Clube, aqui a preparação não para. Aqui", `
+    Escola de Futebol S.E.Atlantico, franquia oficial Prudentópolis Futebol Clube, aqui a preparação não para.
+     Aqui uma amostra do nosso trabalho deste domingo.
+     Comando professor Eduardo e professor Nhopir.
+     Coordenação Prof Josuel Silva`, "../../../public/img/noticias/img5.jpeg"),
+  new Card("REDE DE PROSPERIDADE FAMILIAR e o CIEDS. Muitas novidades virão logo, logo!", `Reunião de apresentação da nossa assistente social, Amanda, e alinhamento com as ações do projeto REDE DE PROSPERIDADE FAMILIAR e o CIEDS. Muitas novidades virão logo, logo!`, "../../../public/img/noticias/img6.jpeg"),
+  new Card("Seminário de Comunicação Comunitária", `Seminário de Comunicação Comunitária promovido pela Rede de Prosperidade Familiar/CIEDS/Vale.`, "../../../public/img/noticias/img7.jpeg"),
 ]
 
 function render() {
-        noticias.map(e => {
-            const renderizer = `
+  noticias.map(e => {
+    const renderizer = `
             <div>
                 <div class="content-card">
                   <div class="img">
@@ -43,9 +44,9 @@ function render() {
               </section>
             </div>
             `
-            display.innerHTML += renderizer;
+    display.innerHTML += renderizer;
 
-        })
+  })
 
 }
 
